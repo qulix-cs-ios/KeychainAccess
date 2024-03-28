efaafad9d28c8d117cf82fe35cc911393247d097
Ivan Tsvirko <tsvirkoiv@qulix.com>
2024-03-28 10:34:09 +0300

    Updated actual minimum deployments

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -12,7 +12,7 @@ import PackageDescription
 let package = Package(
     name: "KeychainAccess",
     platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v4)
     ],
     products: [
         .library(name: "KeychainAccess", targets: ["KeychainAccess"])
